62d05241c096ab9872f8db6de1f394db97ef5b04
Monhoo Batraa <85657308+MonhooBatraa@users.noreply.github.com>
2025-11-08 01:33:10 +0800

    Add files via upload

---
+++ Marta_Core_Engine.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Resources\StepTimer.h"
+#include "Resources\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+
+// Renders Direct3D content on the screen.
+namespace Marta_The_Engine
+{
+	class Marta_Core_Engine
+	{
+	public:
+		Marta_Core_Engine();
+		void CreateRenderers(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		void Update();
+		bool Render();
+
+		void OnWindowSizeChanged();
+		void OnSuspending();
+		void OnResuming();
+		void OnDeviceRemoved();
+
+	private:
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
